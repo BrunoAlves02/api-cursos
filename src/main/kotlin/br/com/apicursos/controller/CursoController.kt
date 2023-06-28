8353877e5ad59a9f81d109a4186ca9f4e6d60d98
Bruno Alves <brunoguilherme0097@gmail.com>
2023-06-28 12:02:08 -0300

    Adicionando métodos CRUD no controller e classes de exceção

--- src/main/kotlin/br/com/apicursos/controller/CursoController.kt
+++ src/main/kotlin/br/com/apicursos/controller/CursoController.kt
@@ -4,9 +4,7 @@ import br.com.apicursos.model.Curso
 import br.com.apicursos.service.CursoService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/curso")
@@ -21,9 +19,30 @@ class CursoController {
         return service.findAll()
     }
 
-    @RequestMapping
-    fun findById(id : Long): Curso{
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable(value = "id") id : Long): Curso{
         return service.findById(id)
+    }
+
+    @RequestMapping(method = [RequestMethod.POST],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun create(@RequestBody curso: Curso): Curso {
+        return service.create(curso)
+    }
+
+    @RequestMapping(method = [RequestMethod.PUT],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun update(@RequestBody curso: Curso): Curso{
+        return service.update(curso)
+    }
+
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun delete(@PathVariable(value = "id") id : Long){
+        return service.delete(id)
     }
 
 }
